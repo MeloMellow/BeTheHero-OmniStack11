11e325cea0d4d03d5945d2725c75a6b8abca3e3f
Rafael Melo <mellowteam@hotmail.com>
2020-03-28 18:11:32 -0300

    mobile app non-api logic finalized

--- mobile-frontend/src/pages/Detail/index.js
+++ mobile-frontend/src/pages/Detail/index.js
@@ -1,43 +1,66 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, TouchableOpacity, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity, Text, Image, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Detail(){
+
+    const navigation = useNavigation();
+
+    const message = 'Hello APAD, I want to help on the "Case" with the value of R$ 120,00'
+
+    function navigateBack(){
+        navigation.goBack();
+    }
+
+    function sendMail(){
+        MailComposer.composeAsync({
+            subject: 'Hero of the case...',
+            recipients: ['diego@rocketseat.com.br'],
+            body: message
+        });
+    }
+
+    function sendWhatsapp(){
+        Linking.openURL(`whatsapp://send?phone=4424124124&text=${message}`);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
-                <TouchableOpacity>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#E82041" />
                 </TouchableOpacity>
             </View>
 
             <View style={styles.incident}>
-                <Text style={styles.incidentProperty}>ONG: </Text>
+                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG: </Text>
                 <Text style={styles.incidentValue}>APAD</Text>
                 <Text style={styles.incidentProperty}>CASE: </Text>
                 <Text style={styles.incidentValue}>Accident</Text>
                 <Text style={styles.incidentProperty}>VALUE: </Text>
                 <Text style={styles.incidentValue}>R$ 120,00</Text>
-                <View style={styles.contactBox}>
-                    <Text style={styles.heroTitle}>Save the day!</Text>
-                    <Text style={styles.heroTitle}>Be the hero of this case.</Text>
+            </View>
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Save the day!</Text>
+                <Text style={styles.heroTitle}>Be the hero of this case.</Text>
 
-                    <Text style={styles.heroDescription}>Contact:</Text>
+                <Text style={styles.heroDescription}>Contact:</Text>
 
-                    <View style={styles.actions}>
-                        <TouchableOpacity style={styles.action} onPress={()=>{}} >
-                            <Text style={styles.actionText}>WhatsApp</Text>
-                        </TouchableOpacity>
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp} >
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
 
-                        <TouchableOpacity style={styles.action} onPress={()=>{}} >
-                            <Text style={styles.actionText}>E-mail</Text>
-                        </TouchableOpacity>
-                    </View>
+                    <TouchableOpacity style={styles.action} onPress={sendMail} >
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
--- mobile-frontend/src/pages/Incidents/styles.js
+++ mobile-frontend/src/pages/Incidents/styles.js
@@ -38,7 +38,7 @@ export default StyleSheet.create({
         padding: 24,
         borderRadius: 8,
         backgroundColor: '#FFF',
-        marginBottom: 16
+        marginBottom: 16,
     },
     incidentProperty: {
         fontSize: 14,
